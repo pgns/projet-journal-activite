f90153b7b89dda3c51a41248f38414709ac620f3
PizzalaFlorian <florian.pizzala@gmail.com>
2016-02-03 16:20:51 +0100

    maj Insert user (warning)
    need d'avoir renomer la table sujet en candidat

--- BDD/insert_User.sql
+++ BDD/insert_User.sql
@@ -5,3 +5,12 @@ INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) 
 INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) VALUES (005,'JM','chercheur','JM','test@gmail.com');
 INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) VALUES (006,'trouve','chercheur','trouve','test@gmail.com');
 INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) VALUES (007,'James','admin','Bond','test@gmail.com');
+
+INSERT INTO `candidat`(`CodeCandidat`, `Age`, `GenreCandidat`, `LieuxEtude`, `NiveauEtude`, `DiplomePrep`, `EtatCivil`, `NombreEnfant`, `ID`) 
+VALUES (001,22,'vache','Grenoble','CAP','Master Macrame','Decede',12,001);
+INSERT INTO `candidat`(`CodeCandidat`, `Age`, `GenreCandidat`, `LieuxEtude`, `NiveauEtude`, `DiplomePrep`, `EtatCivil`, `NombreEnfant`, `ID`) 
+VALUES (002,19,'tortue','Suez','BAC +2','Master Droit','En couple',0,002);
+INSERT INTO `candidat`(`CodeCandidat`, `Age`, `GenreCandidat`, `LieuxEtude`, `NiveauEtude`, `DiplomePrep`, `EtatCivil`, `NombreEnfant`, `ID`) 
+VALUES (003,26,'homme','Vienne','BAC+7','L1 jardinage','Celibataire',1,003);
+INSERT INTO `candidat`(`CodeCandidat`, `Age`, `GenreCandidat`, `LieuxEtude`, `NiveauEtude`, `DiplomePrep`, `EtatCivil`, `NombreEnfant`, `ID`) 
+VALUES (004,11,'chien','Paris','MatSup','CNRS','Union libre',4,004);

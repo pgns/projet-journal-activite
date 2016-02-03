950427f301c574f3fa3f875e445e6846fad89529
PizzalaFlorian <florian.pizzala@gmail.com>
2016-02-03 14:17:08 +0100

    candidat sidebar + rangement

---
+++ includes/CandidatSidebar.inc.php
@@ -0,0 +1,23 @@
+<div id="sidebar">
+
+	<!-- Logo -->
+		<h1 id="logo"><a href="index.html">Journal d'activité</a></h1>
+
+	<!-- Nav -->
+		<nav id="nav">
+			<ul>
+				<li><a href="candidat.php">Accueil</a></li>
+				<li><a href="activites.php">Activités</a></li>
+				<li><a href="monHistorique.php">Mon Historique</a></li>
+				<li><a href="actualite.php">Actualités</a></li>
+				<li><a href="butExperience.php">But de l'expérience</a></li>
+				<li><a href="aide.php">Aide</a></li>
+				<li><a href="monCompte">Mon Compte</a></li>
+			</ul>
+		</nav>
+	<!-- Copyright -->
+		<ul id="copyright">
+			<li>&copy; Untitled.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+		</ul>
+
+</div>

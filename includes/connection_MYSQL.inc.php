8b874f2cbaccad2bf78a85eb8a459c4ea91a278e
PizzalaFlorian <florian.pizzala@gmail.com>
2016-02-03 13:36:30 +0100

    SAAAAALE

--- includes/connection_MYSQL.inc.php
+++ includes/connection_MYSQL.inc.php
@@ -3,6 +3,6 @@
 	try	{
     $bdd = new PDO('mysql:host=localhost;dbname=bdd;charset=utf8', 'root');
 	}catch(Exception $e){
-        die('Erreur : Impossible de se connecter.'.$e->getMessage());
+        $bdd = new PDO('mysql:host=localhost;dbname=bdd;charset=utf8', 'GNS','gns');
 	}
 ?>

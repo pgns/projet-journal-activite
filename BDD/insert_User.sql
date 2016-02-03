f43615cf501125065b7385e91893bd3fe94390f4
PizzalaFlorian <florian.pizzala@gmail.com>
2016-02-03 16:08:54 +0100

    Script Insertion User BDD

---
+++ BDD/insert_User.sql
@@ -0,0 +1,7 @@
+INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) VALUES (001,'test','candidat','test','test@gmail.com');
+INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) VALUES (002,'toto','candidat','toto','test@gmail.com');
+INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) VALUES (003,'boule','candidat','boule','test@gmail.com');
+INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) VALUES (004,'bill','candidat','bill','test@gmail.com');
+INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) VALUES (005,'JM','chercheur','JM','test@gmail.com');
+INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) VALUES (006,'trouve','chercheur','trouve','test@gmail.com');
+INSERT INTO `utilisateur`(`ID`, `Login`, `TypeUser`, `MotDePasse`, `MailSujet`) VALUES (007,'James','admin','Bond','test@gmail.com');

fcec701fe200c05e517c7acddf279a51804c6f2e
Barna <csomorbarnabas@yahoo.com>
2016-03-31 21:01:04 +0200

    Incription candidat html
    Ajout recapcha google api, il faudra changer les clef privé et public
    lors de la mise en ligne, nom de domaine non vérifié

--- controleur/connection.ctrl.php
+++ controleur/connection.ctrl.php
@@ -30,6 +30,51 @@
 			$reponse="Erreur : login ou mots de passe invalide";
 		}
 	}
+	
+	
+	
+	
+//fonction pour vérifier le captcha	
+function isRecaptchaValid($code, $ip = null){
+	if (empty($code)) {
+		return false; // Si aucun code n'est entré, on ne cherche pas plus loin
+	}
+	$params = [
+		'secret'    => '6LdZQBwTAAAAAHjzumX5_kPaLF3_bspnqD6lJjDv',
+		'response'  => $code
+	];
+	if( $ip ){
+		$params['remoteip'] = $ip;
+	}
+	$url = "https://www.google.com/recaptcha/api/siteverify?" . http_build_query($params);
+	if (function_exists('curl_version')) {
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // Evite les problèmes, si le ser
+		$response = curl_exec($curl);
+	} else {
+		// Si curl n'est pas dispo, un bon vieux file_get_contents
+		$response = file_get_contents($url);
+	}
+	if (empty($response) || is_null($response)) {
+		return false;
+	}
+	$json = json_decode($response);
+	return $json->success;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	require_once('../includes/head.inc.php');
 	require_once('../vue/connection.vue.php');
 	//require_once('../includes/menu.inc.php');
--- vue/connection.vue.php
+++ vue/connection.vue.php
@@ -15,9 +15,81 @@
 				</p>
 				<p><?php echo $reponse;?></p>
 			</form>
-
+			
+			
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+			Pas encore inscrit?<br/>
+			<a class="js-scrollTo" href="#inscription"><button>Je m'inscrit !</button></a>
 		</center>
+		Bienvenu sur cet étude!<br/>
+		Le but de cet étude est de bla bla...<br/>
+		Grace à vous la science progresse bla bla...<br/>
+		
+		<!-- A mettre dans le css-->
+		<div style="margin-bottom:700px;"></div>
+		
+		<form id="inscription" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post" accept-charset="UTF-8">
+			<label>Entrez votre e-mail:</label>
+			<input type="text" name="mail" required/>
+			<label>Entrez un login: </label>
+			<input type="text" name="login" required/>
+			<label>Entrez un mot de passe:</label>
+			<input type="password" name="mdp" required/>
+			<label>Confirmez le mot de passe:</label>
+			<input type="password" name="new_mdp" required/>
+			<label>Entrez votre age:</label>
+			<input type="number" name="age" value="20" required/><br/>
+			<label>Sélectionnez le niveau d'étude:</label>
+			<select name="dip">
+				<option value="Bac +1">Bac +1</option>
+				<option value="Bac +2">Bac +2</option>
+				<option value="Bac +3">Bac +3</option>
+				<option value="Bac +4">Bac +4</option>
+				<option value="Bac +5">Bac +5</option>
+				<option value="Bac +6">Bac +6</option>
+				<option value="Bac +7">Bac +7</option>
+				<option value="Bac +8">Bac +8</option>
+			</select>
+			<label>Sélectionnez le diplôme que vous êtes en train de préparer:</label>
+			<select name="dip">
+				<option value="Licence">Licence</option>
+				<option value="Master">Master</option>
+				<option value="Doctorat">Doctorat</option>
+				<option value="BTS">BTS</option>
+				<option value="DUT">DUT</option>
+				<option value="Diplôme d'ingénieur">Diplôme d'ingénieur</option>
+				<option value="Diplôme médical et paramédical">Diplôme médical et paramédical</option>
+				<option value="Autre diplôme">Autre diplôme </option>
+			</select>
+			<label>Sélectionnez votre état civil:</label>
+			<select name="civ">
+				<option value="Marié(e)">Marié(e)</option>
+				<option value="Pacsé(e)">Pacsé(e)</option>
+				<option value="Non marié(e) avec partenaire stable">Non marié(e) avec partenaire stable</option>
+				<option value="Non marié(e) sans partenaire stable">Non marié(e) sans partenaire stable</option>
+				<option value="Veuve, veuf">Veuve, veuf</option>
+				<option value="Divorcé">Divorcé</option>
+			</select>
+			<label>Entrez le nombre d'enfants:</label>
+			<input type="number" name="nb" required value="0"/><br/>
+			<label>Entrez votre lieu d'étude:</label>
+			<input type="text" name="lieu" required/>
+			<script src="https://www.google.com/recaptcha/api.js?hl=fr" async defer></script>
+			<div class="g-recaptcha" data-sitekey="6Le7BxoTAAAAAIWC3WoQMB5CFsHrXRPozy2DSQ32"></div><br/>
+			<input type="submit" name="incription" value="Inscription"/>
+		</form>
 	</div>
+	
+	<script>
+	$(document).ready(function() {
+		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 550; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
+	});
+	</script>
+	
 	
 </div>

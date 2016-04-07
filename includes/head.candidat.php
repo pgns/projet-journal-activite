bd93e6d45f49f1782bcd39170e41c3d0ab0f4d94
PizzalaFlorian <florian.pizzala@gmail.com>
2016-04-07 15:26:03 +0200

    Insertion calendar dans activite candidat
    Attention encore des bug et pas de synchro sur les dates ou la bdd

--- includes/head.candidat.php
+++ includes/head.candidat.php
@@ -1,13 +1,22 @@
 
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
 	<head>
 		<title>Accueil</title>
-		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" media="screen" type="text/css" title="Design" href="../assets/css/Demo_calendar_style.css" />
+        <link rel="stylesheet" media="screen" type="text/css" title="Design" href="../assets/css/Demo_calendar_jquery.css" />
+        <script type="text/javascript" src="../js/jquery-1.3.2.min.js"></script>
+        <script type="text/javascript" src="../js/jquery-ui-1.7.2.custom.min.js"></script>
+        <script type="text/javascript" src="../js/Demo_calendar_script.js"></script>
+        <script type="text/javascript" src="../js/jquery.corner.js"></script>
+		
 		<!-- Add jQuery library -->
+<!--
 		<script type="text/javascript" src="../js/fancybox/lib/jquery-1.10.1.min.js"></script>
+-->
 
 		<!-- Add mousewheel plugin (this is optional) -->
 		<script type="text/javascript" src="../js/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>

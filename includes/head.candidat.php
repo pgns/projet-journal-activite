e07c426319d0da01a37f87fa7b7867f822273cff
PizzalaFlorian <florian.pizzala@gmail.com>
2016-04-07 15:30:21 +0200

    header
    Mis en comm de cercertain lien qui font conflit

--- includes/head.candidat.php
+++ includes/head.candidat.php
@@ -19,12 +19,16 @@
 -->
 
 		<!-- Add mousewheel plugin (this is optional) -->
+<!--
 		<script type="text/javascript" src="../js/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+-->
 
 		<!-- Add fancyBox main JS and CSS files -->
+<!--
 		<script type="text/javascript" src="../js/fancybox/source/jquery.fancybox.js?v=2.1.5"></script>
 		<link rel="stylesheet" type="text/css" href="../js/fancybox/source/jquery.fancybox.css?v=2.1.5" media="screen" />
 		
+-->
 		<link rel="stylesheet" href="../assets/css/main.css" />
 		<link rel="stylesheet" href="../assets/css/occupation.css" />
 	</head>
